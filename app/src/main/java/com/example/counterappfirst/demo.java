cb478a46a590681099be3bffd2bf65a21f43c2a0
webgeekhabib007 <73267589+webgeekhabib007@users.noreply.github.com>
2023-03-28 04:42:52 +0600

    demo.java file edited

--- app/src/main/java/com/example/counterappfirst/demo.java
+++ app/src/main/java/com/example/counterappfirst/demo.java
@@ -1,4 +1,4 @@
-//java class with constructor,destructors and methods named demo
+//java class with constructor,destructors and methods named demo (edited in main branch)
 class demo
 {
     int a,b;
